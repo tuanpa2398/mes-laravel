86981784ccd953bdaffeccc8d9057ad8b0a3da8d
PaTuan <tuantuan230298@gmail.com>
2025-10-26 00:24:35 +0700

    coimmit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'MES') }}</title>
+        <link rel="stylesheet" href="{{ asset('assets/css/tailwind.min.css') }}" />
+    </head>
+    <body class="">
+        <span class="text-amber-700 text-[40px]">123213123213</span>
+    </body>
+</html>
